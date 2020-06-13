7aec76c3daf544f9af115009646fb60112bc14c8
youngua <gyoung@risd.edu>
2020-06-13 01:04:58 -0400

    dropdown menu implemented

--- script.js
+++ script.js
@@ -5,6 +5,15 @@
 *The code below - when the right or left arrows are clicked, the entire html page (each is its own slideshow), turns to the next page.
 */
 
+function toggleMenu() {
+    var x = document.getElementById("dropdown-container");
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+    } else {
+        x.style.display = "none";
+    }
+}
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
