6a8660a1e72d185f651579c3fdea11d7dab57fc6
youngua <gyoung@risd.edu>
2020-07-04 23:34:40 -0400

    update header scroll

--- script.js
+++ script.js
@@ -20,11 +20,15 @@ function toggleMenu() {
 
 /*
  * Defines the media queries for small screens
+ * mobileP = Portrait
+ * mobileL = Landscape
  */
-var mobileQ = [
+var mobileP = [
     window.matchMedia("(min-height: 1084px)"),
     window.matchMedia("(max-width: 768px)")
 ]
+
+var mobileL = window.matchMedia("(max-width: 2000px)");
 
 /*
  * Defines what should happen for small screens
@@ -32,7 +36,7 @@ var mobileQ = [
 $(document).ready(function () {
 
     // triggers the menu dropdown
-    if (mobileQ[0].matches || mobileQ[1].matches) {
+    if (mobileP[0].matches || mobileP[1].matches) {
 
         $(".toggle").hide();
         $(".mobile-nav").click(function () {
@@ -51,17 +55,78 @@ $(document).ready(function () {
 var elementPosition = $('header').offset();
 
 $(window).scroll(function () {
-    if ($(window).scrollTop() > elementPosition.top) {
-        $('header').css({ 'position': 'fixed', 'top': '0', 'width': '88%', 'z-index': '2' });
+
+    // if the page is portrait then make header scrollable
+    if ($(window).scrollTop() > elementPosition.top && mobileP[0].matches || mobileP[1].matches) {
+        $('header').css({
+            'position': 'fixed',
+            'top': '0',
+            'left': '0',
+            'right': '0',
+            'padding': '2% 2%',
+            'z-index': '2'
+        });
+
+        $('#dialogues-header').css({
+            'margin-left': '2%',
+        });
+
+        $('#spring-right').css({
+            'margin-right': '2%',
+        });
 
     } else {
-        $('header').css({ 'position': 'static', 'width': 'auto' });
+        $('header').css({
+            'position': 'static',
+            'width': 'auto',
+            'padding': '2% 2%',
+        });
+    }
+
+    // if the page is landscape then make header scrollable
+    if ($(window).scrollTop() > elementPosition.top && mobileL.matches) {
+
+        $('header').css({
+            'position': 'fixed',
+            'top': '0',
+            'width': '100%',
+            'padding': '1% 2%',
+            'z-index': '2'
+        });
+
+        $('#spring-right').css({
+            'margin-right': '4%',
+        });
+    } else {
+
+        $('header').css({
+            'position': 'static',
+            'width': 'auto',
+            'padding': '1% 2%'
+        });
+
+        $('#spring-right').css({
+            'margin-right': 'auto',
+        });
     }
 });
 
-// Reloads webpage when mobile orientation changes
+/*
+ * Reloads webpage when mobile orientation changes
+ */
 window.onorientationchange = function () {
     window.onorientationchange = function () {
         window.location.reload();
     }
 };
+
+/*
+ * disable page scroll when scrolling a div
+ */
+var $div = $('#dropdown-container');
+$div.on('mousewheel DOMMouseScroll', function (e) {
+    var d = e.originalEvent.wheelDelta || -e.originalEvent.detail,
+        dir = d > 0 ? 'up' : 'down',
+        stop = (dir == 'up' && this.scrollTop == 0) || (dir == 'down' && this.scrollTop == this.scrollHeight - this.offsetHeight);
+    stop && e.preventDefault();
+});
