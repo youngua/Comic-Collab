2ffdbf1f045bb1571efa16f7e8f040b23a916a21
youngua <gyoung@risd.edu>
2020-07-13 17:33:53 -0400

    update header respond

--- script.js
+++ script.js
@@ -12,15 +12,6 @@ function toggleMenu() {
 }
 
 /*
- * Defines the media queries for small screens
- */
-var mobile = [
-    window.matchMedia("(min-height: 1084px)"),
-    window.matchMedia("(max-width: 768px)"),
-    window.matchMedia("(max-height: 414px)")
-]
-
-/*
  * Defines what should happen for small screens
  */
 $(document).ready(function () {
@@ -41,19 +32,19 @@ $(document).ready(function () {
     }
 });
 
-// side note: may have to implement a condition for browser check https://stackoverflow.com/questions/4565112/javascript-how-to-find-out-if-the-user-browser-is-chrome/13348618#13348618
-
 /*
  * Defines the media queries for small screens
  */
-// var mobileR = [
-//     window.matchMedia("(max-height: 1400px)"),
-//     window.matchMedia("(max-width: 1024px)"),
+var mobile = [
+    window.matchMedia("(min-height: 1084px)"),
+    window.matchMedia("(max-width: 768px)"),
+    window.matchMedia("(max-height: 414px)"),
 
-//     window.matchMedia("(min-width: 800px)")
-// ]
+    window.matchMedia("(max-width: 2000px)"),
+]
 
-var mobileL = window.matchMedia("(max-width: 2000px)");
+// checks to see if browser is safari
+// var is_safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
 /*
  * fixed the header when user scroll
@@ -62,68 +53,39 @@ var elementPosition = $('header').offset();
 
 $(window).scroll(function () {
 
-    // if the page scroll then fixed the header to the top
+
     if ($(window).scrollTop() > elementPosition.top) {
 
-        // if the page is portrait then make header scrollable
-        if (mobile[0].matches || mobile[1].matches) {
-            $('header').css({
-                'position': 'fixed',
-                'left': '0',
-                'right': '0',
-                'padding': '2% 2%',
-            });
-
-            $('#dialogues-header').css({
-                'margin-left': '2%',
-            });
-
-            $('#spring-right').css({
-                'margin-right': '-7%',
-            });
-
-        }
-    } else {
-
-        $('header').css({
-            'position': 'static',
-            'width': 'auto',
-        });
-
-        $('#spring-right').css({
-            'margin-right': '2.2%',
-        });
-
-    }
-
-    // mac vs chrome issue
-    if ($(window).scrollTop() > elementPosition.top) {
-
-        // if the page is landscape then make header scrollable
-        if (mobileL.matches) {
+        if (mobile[3].matches) {
 
             $('header').css({
                 'position': 'fixed',
                 'width': '100vw',
-                // 'padding-right': '10vw',
+            });
+
+            $('#dialogues-header').css({
+                'position': 'fixed',
             });
 
             $('#spring-right').css({
-                'margin-right': '8vw',
+                'position': 'fixed',
             });
-        }
 
+        }
     } else {
 
         $('header').css({
             'position': 'static',
-            'width': 'auto',
+            'width': '100vw',
+        });
+
+        $('#dialogues-header').css({
+            'position': 'fixed',
         });
 
         $('#spring-right').css({
-            'margin-right': '0',
+            'position': 'fixed',
         });
-
     }
 
 });
