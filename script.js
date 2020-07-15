ad3d8bbd6a267c0f236b37b3d6333aa5e87496af
youngua <gyoung@risd.edu>
2020-07-14 21:57:44 -0400

    update

--- script.js
+++ script.js
@@ -109,14 +109,7 @@ function orientationChange() {
 /*
 * Locks the orientation when in Fullscreen mode
 */
-$(document).ready(function () {
-    function reorient(e) {
-        var portrait = (window.orientation % 180 == 0);
-        $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
-    }
-    window.onorientationchange = reorient;
-    window.setTimeout(reorient, 0);
-});
+
 
 
 /*
